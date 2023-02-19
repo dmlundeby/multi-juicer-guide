e91099d458c861cbdf96b05ce02b25a50cc2ddc2
Daniel Lundeby <daniel.lundeby@bouvet.no>
2023-02-19 13:11:12 +0100

    Bugfix

--- setup_06_fqdn.sh
+++ setup_06_fqdn.sh
@@ -1,3 +1,3 @@
 EXTERNAL_IP="$(kubectl get services --namespace default nginx-ingress-ingress-nginx-controller --output jsonpath='{.status.loadBalancer.ingress[0].ip}')"
-PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$EXTERNAL_IP')].[id]" --output tsv)
 az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
