d497b119ac96b65ee6a5f4ea9f27c98d79d52ffe
Daniel Lundeby <daniel.lundeby@bouvet.no>
2023-02-21 01:50:21 +0100

    Use values.yaml

--- step_02_helm_multijuicer.sh
+++ step_02_helm_multijuicer.sh
@@ -1,2 +1,2 @@
 helm repo add multi-juicer https://iteratec.github.io/multi-juicer/
-helm install multi-juicer multi-juicer/multi-juicer
+helm install -f values.yaml multi-juicer multi-juicer/multi-juicer
